28e8813af617f1f540a9bf4307f75d3a15a41a9e
xxhfg <xxhfg@163.com>
2013-02-16 17:12:47 +0800

    new

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+"""
+将LIB目录下引用包加入系统路径
+"""
 
 import  os
 import sys
@@ -13,5 +16,51 @@ sys.path.append(LIB_DIR)
 dirs = [n for n in os.listdir(LIB_DIR) if
         os.path.isdir(os.path.join(LIB_DIR, n))]
 for d in dirs:
-	sys.path.append(os.path.join(LIB_DIR, d))
-print map(__import__,dirs)
+    sys.path.append(os.path.join(LIB_DIR, d))
+
+import tornado
+import tornado.web
+from tornado.options import define, options
+
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "static"),
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=True, type=bool)
+define("xsrf_cookies", default=True, type=bool)
+define("cookie_secret", '')
+define("login_url", '/login')
+define("static_url_prefix", '/static/')
+
+ui_modules = {}
+settings = dict(
+    debug=options.debug,
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    # xsrf_cookies=options.xsrf_cookies,
+    # cookie_secret=options.cookie_secret,
+    # login_url=options.login_url,
+    # static_url_prefix=options.static_url_prefix,
+
+    ui_modules=ui_modules,
+
+    # auth secret
+    # twitter_consumer_key=options.twitter_consumer_key,
+    # twitter_consumer_secret=options.twitter_consumer_secret,
+    # friendfeed_consumer_key=options.friendfeed_consumer_key,
+    # friendfeed_consumer_secret=options.friendfeed_consumer_secret,
+    # facebook_api_key=options.facebook_api_key,
+    # facebook_secret=options.facebook_secret,
+)
+
+handlers = [
+    (r"/(favicon\.ico)", tornado.web.StaticFileHandler,
+     dict(path=settings['static_path'])),
+    (r"/static/(.+)", tornado.web.StaticFileHandler,
+     dict(path=MEDIA_ROOT, name='static_path')),
+    (r"/media/(.+)", tornado.web.StaticFileHandler,
+     dict(path=MEDIA_ROOT, name='media_path')),
+]
+# handlers.append(('.*', tornado.web.FallbackHandler,
+                 # dict(fallback=WSGIContainer(WSGIHandler()))))
+# handlers = []
+sub_handlers = []
--- serv.py
+++ serv.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 
+from config import *
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -9,7 +10,18 @@ import tornado.web
 
 from tornado.options import define, options
 
-define("port", default=8888, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        # from poweredsites.urls import handlers, sub_handlers, ui_modules
+
+        handlers.append((r"/", MainHandler))
+        super(Application, self).__init__(handlers, **settings)
+
+        # add handlers for sub domains
+        # for sub_handler in sub_handlers:
+            # host pattern and handlers
+            # self.add_handlers(sub_handler[0], sub_handler[1])
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -19,9 +31,10 @@ class MainHandler(tornado.web.RequestHandler):
 
 def main():
     tornado.options.parse_command_line()
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    # application = tornado.web.Application([
+        # (r"/", MainHandler),
+    # ])
+    application = Application()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
